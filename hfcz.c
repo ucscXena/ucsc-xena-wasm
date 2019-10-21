33711caf1a811d94f41aa1e29aff76bfd142e2e8
Brian Craft <craft@soe.ucsc.edu>
2019-10-21 18:02:29 +0000

    Add compress & error handling to cli tool

--- hfcz.c
+++ hfcz.c
@@ -5,28 +5,95 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #include "hfc.h"
+#include "queue.h"
+#include "bytes.h"
 
 void usage() {
 	printf("hfcz [-u] <file>\n");
 	exit(1);
 }
 
-void compress(char *filename) {
-	// probably not going to fix this, since we're dropping hfc for hfc.
-	printf("fixme\n");
-	exit(1);
+int compress(const char *filename) {
+	int fd;
+	struct stat stat;
+	int r;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		return -1;
+	}
+
+	r = fstat(fd, &stat);
+	if (r == -1) {
+		return -1;
+	}
+
+	size_t size = stat.st_size;
+	uint8_t *buff = malloc(size + 1);
+	if (buff == NULL) {
+		return -1;
+	}
+
+	r = read(fd, buff, size);
+	if (r == -1) {
+		return -1;
+	}
+	buff[size] = '\0'; // handle missing final newline
+	int lines = 0;
+	for (int i = 0; i < size; ++i) {
+		if (buff[i] == '\n') {
+			lines++;
+		}
+	}
+	uint8_t **strs = malloc(sizeof(char *) * lines);
+	if (strs == NULL) {
+		return -1;
+	}
+	uint8_t *c = buff;
+	uint8_t **s = strs;
+	for (int i = 0; i < size; ++i) {
+		if (buff[i] == '\n') {
+			*s = c;
+			++s;
+			buff[i] = '\0';
+			c = buff + i + 1;
+		}
+	}
+	struct bytes *cmp = hfc_compress(lines, strs);
+	r = write(1, cmp->bytes, cmp->len);
+	if (r == -1) {
+		return -1;
+	}
+	free(strs);
+	free(buff);
+	bytes_free(cmp);
+	return 0;
 }
 
 // XXX error handling
-void decompress(char *filename) {
+int decompress(char *filename) {
 	struct stat filestats;
-	stat(filename, &filestats);
+	int r;
+	r = stat(filename, &filestats);
+	if (r == -1) {
+		return -1;
+	}
 	off_t size = filestats.st_size;
 	char *buff = malloc(size);
+	if (buff == NULL) {
+		return -1;
+	}
 	FILE *fp = fopen(filename, "r");
-	size = fread(buff, 1, size, fp);
+	if (fp == NULL) {
+		return -1;
+	}
+	r = fread(buff, 1, size, fp);
+	if (r < size) {
+		return -1;
+	}
 	fclose(fp);
 
 	struct hfc *hfc = hfc_new(buff, size);
@@ -40,21 +107,30 @@ void decompress(char *filename) {
 	hfc_iter_free(iter);
 	hfc_free(hfc);
 	free(buff);
+	return 0;
 }
 
 
 int main(int argc, char *argv[]) {
 	int d = 0;
+	int r;
 	char *filename;
 	if (argc < 2) {
 		usage();
 	}
+
 	if (!strcmp(argv[1], "-u")) {
 		if (argc < 3) {
 			usage();
 		}
-		decompress(argv[2]);
+		r = decompress(argv[2]);
 	} else {
-		compress(argv[1]);
+		r = compress(argv[1]);
 	}
+
+	if (r != 0) {
+		perror("Failed with error");
+		exit(-1);
+	}
+	exit(0);
 }
